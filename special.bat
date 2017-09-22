ef4d7806ee0c602e1f0b87e0ab23e15c0c52068f
Liu-Wen-Yuan <15816141883@163.com>
2017-09-21 22:55:40 -0500

    Update special.bat

--- special.bat
+++ special.bat
@@ -1,5 +1,5 @@
 @echo off
 title special couscous
-echo your os name will be in a folder called os-samples in the c drive or where you installed Git. (i works only if you have git)
+echo the os sample will be in a folder called os-samples in this path. (i works only if you have git)
 Pause
 git clone https://github.com/TemTemmie/os-sample.git
